541e0b123229daaac80631d6f529c9298d938b0a
Chase Blokker <chaseblokker@yahoo.com>
2014-09-23 20:25:07 -0400

    created basic login/signup system for app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902182817) do
+ActiveRecord::Schema.define(version: 20140923234339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 20140902182817) do
     t.string   "weeks_range_52"
     t.string   "day_value_change"
     t.string   "dividend_yield"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.integer  "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
